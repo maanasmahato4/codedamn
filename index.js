86410f9515f061c4c27c618f282342e0356e2238
maanasmahato4 <maanasmahato456@gmail.com>
2024-01-23 03:10:28 +0545

    lesson2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("server running at port: 3000");
+});

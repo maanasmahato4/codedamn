14635f71edc8ac1be2a8478995b75510d756e636
maanasmahato4 <maanasmahato456@gmail.com>
2024-01-23 14:15:33 +0545

    lesson10

--- authentication/auth.controller.js
+++ authentication/auth.controller.js
@@ -1,12 +1,54 @@
 const User = require("../models/user.schema");
+const { hashPassword, passwordMatch } = require("../helpers/bcrypt");
+const { generateAccessToken, generateRefreshToken } = require("../helpers/jwt");
 
 const SignUp = async (req, res) => {
+  try {
+    const user = req.body;
+    const hashedPassword = await hashPassword(user.password);
+    user.password = hashedPassword;
+    const savedUser = await User.create(user);
+    const tokenPayload = {
+      username: savedUser.username,
+      role: savedUser.role,
+    };
+    const access_token = generateAccessToken(tokenPayload);
+    const refresh_token = generateRefreshToken(tokenPayload);
+
+    res.cookie("jwt", refresh_token, { httpOnly: true });
+    return res.status(200).json(access_token);
+  } catch (error) {
+    return res.status(500).json({ error: "error occured during signup" });
+  }
 };
 
 const SignIn = async (req, res) => {
+  try {
+    const user = req.body;
+    const userExist = await User.findOne({ username: user.username });
+    if (!userExist) {
+      return res.status(404).json({ error: "user doesn't exist" });
+    }
+    if (!passwordMatch(user.password, userExist.password)) {
+      return res.sendStatus(401);
+    }
+    const tokenPayload = {
+      username: savedUser.username,
+      role: savedUser.role,
+    };
+    const access_token = generateAccessToken(tokenPayload);
+    const refresh_token = generateRefreshToken(tokenPayload);
+
+    res.cookie("jwt", refresh_token, { httpOnly: true });
+    return res.status(200).json(access_token);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "error occured during signin" });
+  }
 };
 
 const SignOut = (req, res) => {
+    
 };
 
 module.exports = { SignUp, SignIn, SignOut };

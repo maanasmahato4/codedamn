8c3fd051016f0f1eb7d991a92097c735373ca111
maanasmahato4 <maanasmahato456@gmail.com>
2024-01-23 06:29:47 +0545

    lesson6

--- todo/todo.controller.js
+++ todo/todo.controller.js
@@ -1,9 +1,21 @@
 const Todo = require("../models/todo.schema");
 
 const GetTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    return res.status(200).json(todos);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 };
 
 const AddTodo = async (req, res) => {
+  try {
+    const newTodo = await Todo.create(req.body);
+    return res.status(200).json(newTodo);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 };
 
 module.exports = { GetTodos, AddTodo };

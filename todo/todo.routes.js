25347c47e3b24ff11fcbc3eb9a82990f18265d24
Maanas Mahato <139013856+maanasmahato4@users.noreply.github.com>
2024-01-24 00:56:16 +0545

    Update todo.routes.js

--- todo/todo.routes.js
+++ todo/todo.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { GetTodos, AddTodo } = require("./todo.controller");
+const verifyJwt = require("../middlewares/verifyJwt.middleware");
 
 const router = express.Router();
 
 router
   .get("/todo/todos", GetTodos)
-  .post("/todo/new", AddTodo);
+  .post("/todo/new", verifyJwt, AddTodo);
 
 module.exports = router;

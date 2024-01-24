ed0fb874bd0c1a3dc3bacd9d4052157f95caa5f9
Maanas Mahato <139013856+maanasmahato4@users.noreply.github.com>
2024-01-24 14:32:11 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/", (req, res) => {
+  return res.sendStatus(200);
+})
+
 app.listen(3000, () => {
   console.log("server running at port: 3000");
 });
